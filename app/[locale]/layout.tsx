69479eabb2719baf4ab58c958a4bf58cff25026c
叶颖 <140698233+JiayiLuoJarvis@users.noreply.github.com>
2026-04-29 08:14:37 +0800

    fix(layout): 修复 Header 组件的渲染问题

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
       >
         <NextIntlClientProvider locale={locale} messages={messages}>
           <SmoothScrolling>
-            <Header />
+            <Header />1
             {children}
             <Footer />
           </SmoothScrolling>
